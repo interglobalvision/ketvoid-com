ca1f242864106a76b4faf6a20ab68e060c2612fe
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-18 17:29:15 -0500

    make title uppercase

--- single-collection.php
+++ single-collection.php
@@ -30,7 +30,7 @@ if( have_posts() ) {
       ?>
 
       </div>
-      <div class="single-collection-title"><h1><?php the_title(); ?></h1></div>
+      <div class="single-collection-title font-uppercase"><h1><?php the_title(); ?></h1></div>
     </article>
 
 <?php
