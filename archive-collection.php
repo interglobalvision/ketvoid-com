03dba12665c483ebfce95f016111d4e78bb2197f
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-18 20:24:52 -0500

    add class to items

--- archive-collection.php
+++ archive-collection.php
@@ -13,7 +13,7 @@ if( have_posts() ) {
   while( have_posts() ) {
     the_post();
 ?>
-          <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+          <article <?php post_class('archive-collection-item'); ?> id="post-<?php the_ID(); ?>">
             <h2><a href="<?php the_permalink() ?>" class="archive-collection-link"><?php the_title(); ?></a></h2>
             <div class="archive-collection-image-holder grid-row">
               <?php the_post_thumbnail('item-l-3-4x3'); ?>
