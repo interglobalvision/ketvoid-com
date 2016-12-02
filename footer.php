a53f9f835c3cc50e65bc3f50cc01ce2d69c4fa94
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-12-02 13:34:40 -0600

    add cart icon to footer

--- footer.php
+++ footer.php
@@ -1,7 +1,12 @@
   <footer id="footer">
-    <nav class="grid-row justify-between font-uppercase font-size-h2">
-      <a class="menu-item" href="<?php echo site_url('/collection'); ?>">Collections</a>
-      <a class="menu-item" href="<?php echo site_url('/shop'); ?>">Shop</a>
+    <nav class="grid-row justify-between align-items-end font-uppercase font-size-h2">
+      <div>
+        <a class="menu-item" href="<?php echo home_url('/collection'); ?>">Collections</a>
+      </div>
+      <div class="grid-row">
+        <a class="footer-cart" href="<?php echo home_url('/cart'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/dist/cart.png"></a><br>
+        <a class="menu-item" href="<?php echo home_url('/shop'); ?>">Shop</a>
+      </div>
     </nav>
   </footer>
 
