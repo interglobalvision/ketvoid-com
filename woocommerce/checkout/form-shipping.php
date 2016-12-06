40692275ac3e8532238602223e2a225201e0eb22
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-12-05 19:18:45 -0600

    scaffold checkout

---
+++ woocommerce/checkout/form-shipping.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Checkout shipping information form
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/checkout/form-shipping.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 2.2.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+?>
+<div class="woocommerce-shipping-fields grid-column">
+	<?php if ( true === WC()->cart->needs_shipping_address() ) : ?>
+
+		<div class="shipping_address grid-column">
+
+			<div class="grid-item item-s-12">
+				<h3>Shipping Address</h3>
+			</div>
+
+			<?php do_action( 'woocommerce_before_checkout_shipping_form', $checkout ); ?>
+
+			<?php foreach ( $checkout->checkout_fields['shipping'] as $key => $field ) : ?>
+
+					<?php woocommerce_form_field( $key, $field, $checkout->get_value( $key ) ); ?>
+
+			<?php endforeach; ?>
+
+			<?php do_action( 'woocommerce_after_checkout_shipping_form', $checkout ); ?>
+
+		</div>
+
+	<?php endif; ?>
+
+</div>
