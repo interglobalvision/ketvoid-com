d73cadfc63a5d532441fe1916acc906a5fc524e5
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-12-15 17:34:22 -0600

    display footer nav items if published posts

--- footer.php
+++ footer.php
@@ -1,9 +1,22 @@
+<?php 
+$collection_count = wp_count_posts('collection')->publish;
+$product_count = wp_count_posts('product')->publish;
+?>
   <footer id="footer" class="text-shadow">
     <nav class="grid-row justify-between align-items-end font-uppercase font-size-medium font-bold">
       <div>
+<?php 
+if ($collection_count > 0) {
+?>   
         <a class="menu-item" href="<?php echo home_url('/collection'); ?>">Collections</a>
+<?php 
+}
+?>
       </div>
       <div class="grid-row">
+<?php
+if ($product_count > 0) {
+?>
         <a class="menu-item" href="<?php echo home_url('/shop'); ?>">Shop</a>
 <?php
   $cart_count = WC()->cart->get_cart_contents_count();
@@ -13,6 +26,7 @@
         <a class="footer-cart menu-item" href="<?php echo home_url('/cart'); ?>"><?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/icon-cart.svg'); ?><span>&nbsp;<?php echo $cart_count; ?></span></a>
 <?php
   }
+}
 ?>
       </div>
     </nav>
